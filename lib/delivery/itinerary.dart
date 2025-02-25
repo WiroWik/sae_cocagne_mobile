e55fd415cfa7b46ad25a296ba05e13dfc5683af3
Leon LeBerre <leon.le-berre5@etu.univ-lorraine.fr>
2025-02-25 13:51:28 +0100

    Refactor ItineraryPage constructor and clean up main.dart comments

--- lib/delivery/itinerary.dart
+++ lib/delivery/itinerary.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ItineraryPage extends StatelessWidget {
+  const ItineraryPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
